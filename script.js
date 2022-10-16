73a12219d1579f41213d09dd2337e73e9be3640a
Mirxojiakbar <mirxojiakbarb2008@gmail.com>
2022-10-16 09:19:38 +0500

    SwitchCase

---
+++ script.js
@@ -0,0 +1,174 @@
+////////////////2-dars swich()///////////////////////
+// 1-7 gacha butun son berilgan kiritilgan songa mos ravishda hafta kunini so'zlarda ifodolovchi dastur tuzing
+
+// let a = +prompt("Hafta kunini kiriting: ");
+// switch (a) {
+//   case 1:
+//     console.log("Dushanba");
+//     break;
+//   case 2:
+//     console.log("Seshanba");
+//     break;
+//   case 3:
+//     console.log("Chorshanba");
+//     break;
+//   case 4:
+//     console.log("Payshanba");
+//     break;
+//   case 5:
+//     console.log("Juma");
+//     break;
+//   case 6:
+//     console.log("Shanba");
+//     break;
+//   case 7:
+//     console.log("Yakshanba");
+//     break;
+//   default:
+//     console.log("Bunday hafta kuni mavjud emas");
+// }
+
+
+
+// // K butun son berilgan Baho natijalarini chiqaruvchi dastur tuzing. 1-yomon, 2-qoniqarsiz, 3-qoniqarli, 4-yaxshi, 5 - alo
+// let K = +prompt('Bahoingizni kirgazing: ');
+// switch (K){
+//     case 1:
+//         alert('yomon');
+//         break;
+//     case 2:
+//         alert('qoniqarsiz');
+//         break;
+//     case 3:
+//         alert('qoniqarli');
+//         break;
+//     case 4:
+//         alert('Yaxshi');
+//         break;
+//     case 5:
+//         alert('alo');
+//         break;
+//     default:
+//         K = +prompt('BAXOINGIZNI KIRITING: ')
+// }
+
+
+
+
+// // Oy raqami berilgan Kiritilgan oy qaysi faslga tegishli ekanin chiqaruvchi dastur tuzing. M - s 2 - qish
+// let oy_raqami = +prompt('Oy raqamini kiriting: ')
+// switch (oy_raqami){
+//     case 1:
+//         alert('Qish')
+//         break;
+//     case 2:
+//         alert('Qish')
+//         break;
+//     case 12:
+//         alert('Qish')
+//         break;
+
+//     case 3:
+//         alert('Baxor')
+//         break;
+//     case 4:
+//         alert('Baxor')
+//         break;
+//     case 5:
+//         alert('Baxor')
+//         break;
+
+//     case 6:
+//         alert('Yoz')
+//         break;
+//     case 7:
+//         alert('Yoz')
+//         break;
+//     case 8:
+//         alert('Yoz')
+//         break;
+
+//     case 9:
+//         alert('Kuz')
+//         break;
+//     case 10:
+//         alert('Kuz')
+//         break;
+//     case 11:
+//         alert('Kuz')
+//         break;
+//     default:
+//         alert('bunday Oy mavjud emas!')
+// }
+
+
+
+
+// Oy raqami berilgan shu oyda nechta kun borligin aniqlovchi dastur tuzing
+// let oy_raqami = +prompt('Oy raqamini kiriting: ')
+// switch (oy_raqami){
+//     case 1:
+//         alert('Yanvar-31 kun (Qish)')
+//         break;
+//     case 2:
+//         alert('Fevral-29(28) kun (Qish)')
+//         break;
+//     case 12:
+//         alert('Decabr-31 kun (Qish)')
+//         break;
+
+//     case 3:
+//         alert('Mart-31 kun (Baxor)')
+//         break;
+//     case 4:
+//         alert('Aprel-30 kun (Baxor)')
+//         break;
+//     case 5:
+//         alert('May-31 kun (Baxor)')
+//         break;
+
+//     case 6:
+//         alert('Iyun-30 kun (Yoz)')
+//         break;
+//     case 7:
+//         alert('Iyul-31 kun (Yoz)')
+//         break;
+//     case 8:
+//         alert('Avgust-31 kun (Yoz)')
+//         break;
+
+//     case 9:
+//         alert('Sentyabr-30 kun (Kuz)')
+//         break;
+//     case 10:
+//         alert('Oktyabr-31 kun (Kuz)')
+//         break;
+//     case 11:
+//         alert('Noyabr-30 kun (Kuz)')
+//         break;
+//     default:
+//         alert('bunday Oy mavjud emas!')
+// }
+
+
+
+
+// // a, b sonlar berilgan c amal ham berilgan a va b sonlar ustida amal bajaruvchi dastur tuzing
+// let a = +prompt("A son:");
+// let b = +prompt("B son:");
+// let amal = prompt("Amal:");
+// switch (amal) {
+//   case "+":
+//     alert('Javobi:'+a + b);
+//     break;
+//   case "-":
+//     alert('Javobi:'+a - b);
+//     break;
+//   case "*":
+//     alert('Javobi:'+a * b);
+//     break;
+//   case "/":
+//     alert('Javobi:'+a / b);
+//     break;
+// }
+
